60c5cfa9fe1f15b563dd6b91ae9c7ee6628710be
Lucas <lucasantony815@gmail.com>
2022-09-02 23:57:20 -0300

    a

--- src/components/workflow/dashboard/Welcome/index.js
+++ src/components/workflow/dashboard/Welcome/index.js
@@ -1,7 +1,29 @@
 import { Avatar } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
 
 export function Welcome() {
+  const [o, setO] = useState(
+    new Date().toLocaleTimeString("pt-BR", { timeZone: "America/Belem" })
+  );
+  useEffect(() => {
+    var myVar = setInterval(myTimer, 1000);
+
+    function myTimer() {
+      var d = new Date(),
+        displayDate;
+      if (navigator.userAgent.toLowerCase().indexOf("firefox") > -1) {
+        displayDate = d.toLocaleTimeString("pt-BR");
+      } else {
+        displayDate = d.toLocaleTimeString("pt-BR", {
+          timeZone: "America/Belem",
+        });
+      }
+      setO(displayDate);
+      console.log(o);
+    }
+  });
+
   return (
     <div className={styles.userData}>
       <div className={styles.headerUserData}>
@@ -14,7 +36,7 @@ export function Welcome() {
         </div>
         <div className={styles.intradayData}>
           <div className={styles.currentIntraday}>
-            <p className={styles.intradayHours}>16:40:60</p>
+            <p className={styles.intradayHours}>{o}</p>
             <p className={styles.day}>21/07/2022</p>
             <span>
               <span className="material-symbols-outlined">clear_day</span>
